93ed2767a8336b8b7b00909d6007feda1e76ed8a
Plabon Datta <plabondatta26@gmail.com>
2020-10-17 08:02:02 +0600

    ready to deploy

--- Player/urls.py
+++ Player/urls.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-import app, UserControl
+from app import views as app
+from UserControl import views as user
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
-    path('',include('UserControl.urls')),
+
+    path('', app.showvideo, name='home'),
+    path('upload/',app.File_upload, name='upload'),
+    path('play/<int:id>/', app.play_video, name='play'),
+    path('reply/<int:vedioId>/<int:id>/', app.CommentReply, name='reply'),
+    path('delete/<int:id>/', app.DeleteView, name= 'delete'),
+    path('add_rating/<int:video_id>/', app.add_ratting, name='add_rating'),
+    path('delete_comment/<int:id>/', app.DeleteComment, name='delete_comment'),
+
+
+    path('login/', user.Loginview, name='login'),
+    path('logout/', user.LogoutView, name='logout'),
+    path('register/', user.RegisterView, name='register'),
+    path('profile/', user.Profile, name='profile'),
+    path('profile/update/', user.UpdateProfile, name='update_profile'),
+    path('profile/change_pp/', user.Profile_pic_upload, name='change_p_pic'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
